9be66cb144c7de020b74f0edbe522477f71031f8
recreational-projects <205627218+recreational-projects@users.noreply.github.com>
2025-10-10 09:56:37 +0100

    Features (#4)
    * add: World.center
    
    * add: Entity.distance_to_squared()
    
    * add Entity.stop()
    
    * add maths.mean_vector()

---
+++ src/flatlandian/maths.py
@@ -0,0 +1,16 @@
+"""Contains general maths."""
+
+from __future__ import annotations
+
+from statistics import fmean
+from typing import TYPE_CHECKING
+
+from pygame import Vector2
+
+if TYPE_CHECKING:
+    from collections.abc import Iterable
+
+
+def mean_vector(vecs: Iterable[Vector2]) -> Vector2:
+    """Return mean vector of `vecs`."""
+    return Vector2(fmean(vec.x for vec in vecs), fmean(vec.y for vec in vecs))
