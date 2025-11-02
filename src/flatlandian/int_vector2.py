35588b8841c65782c1b9c7e508a2d0a393e7dc18
recreational-projects <205627218+recreational-projects@users.noreply.github.com>
2025-11-02 16:39:53 +0000

    rename IntegerVector2 -> IntVector2, add IntVector2 +/- IntPoint

---
+++ src/flatlandian/int_vector2.py
@@ -0,0 +1,50 @@
+"""Contains `IntVector2` class."""
+
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import TYPE_CHECKING
+
+import pygame as pg
+
+if TYPE_CHECKING:
+    from pygame.typing import IntPoint
+
+
+
+@dataclass(frozen=True)
+class IntVector2:
+    """Integer point."""
+
+    x: int
+    """x coordinate."""
+    y: int
+    """y coordinate."""
+
+    @classmethod
+    def from_tuple(cls, value: tuple[int, int]) -> IntVector2:
+        """For compatibility with Pygame functions."""
+        return cls(value[0], value[1])
+
+    @property
+    def as_vector2(self) -> pg.Vector2:
+        """For compatibility with Pygame functions."""
+        return pg.Vector2(self.x, self.y)
+
+    def __add__(self, other: IntVector2 | IntPoint) -> IntVector2:
+        if isinstance(other, IntVector2):
+            return IntVector2(self.x + other.x, self.y + other.y)
+
+        return IntVector2(self.x + other[0], self.y + other[0])
+
+    def __sub__(self, other: IntVector2 | IntPoint) -> IntVector2:
+        if isinstance(other, IntVector2):
+            return IntVector2(self.x - other.x, self.y - other.y)
+
+        return IntVector2(self.x - other[0], self.y - other[0])
+
+    def __mul__(self, other: int) -> IntVector2:
+        return IntVector2(other * self.x, other * self.y)
+
+    def __floordiv__(self, other: int) -> IntVector2:
+        return IntVector2(self.x // other, self.y // other)
