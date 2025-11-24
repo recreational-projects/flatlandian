007ae57bc6aa97ab8ecdd8a17fcceaaa5976eea3
recreational-projects <205627218+recreational-projects@users.noreply.github.com>
2025-11-24 09:53:30 +0000

    wip

--- src/flatlandian/int_vector2.py
+++ src/flatlandian/int_vector2.py
@@ -2,21 +2,13 @@
 
 from __future__ import annotations
 
-from dataclasses import dataclass, fields
-from typing import TYPE_CHECKING
+from dataclasses import dataclass
+from typing import TYPE_CHECKING, Sequence
 
-from pygame import Vector2
+import pygame as pg
 
 if TYPE_CHECKING:
-    from collections.abc import Sized
-
-
-def _ensure_2_elements(value: Sized) -> bool:
-    if len(value) != 2:  # noqa: PLR2004
-        err_msg = f"Expected 2 elements, got {len(value)} elements."
-        raise TypeError(err_msg)
-
-    return True
+    from collections.abc import Iterable
 
 
 @dataclass(frozen=True)
@@ -29,6 +21,8 @@ class IntVector2:
 
     `IntVector2(int, int) -> IntVector2`
 
+    `IntVector2((int, int)) -> IntVector2`
+
     Attempting to construct with `float` parameters raises `TypeError`.
 
     Equivalents to these `pygame.Vector2` constructors aren't yet supported:
@@ -36,8 +30,6 @@ class IntVector2:
     `Vector2(int) -> Vector2`
 
     `Vector2(Vector2) -> Vector2`
-
-    `Vector2((x, y)) -> Vector2`
     """
 
     x: int = 0
@@ -45,52 +37,52 @@ class IntVector2:
     y: int = 0
     """y coordinate."""
 
-    def __post_init__(self) -> None:
-        if not isinstance(self.x, int) or not isinstance(self.y, int):
-            err_msg = f"Expected 2 `int`s; got x={self.x}, y={self.y}"  # type: ignore[unreachable]
-            raise TypeError(err_msg)
 
-    @classmethod
-    def from_tuple(cls, value: tuple[int, int]) -> IntVector2:
-        """Construct vector from `tuple` of `int`s.
 
-        For compatibility with Pygame functions.
-        """
-        _ensure_2_elements(value)
-        return cls(value[0], value[1])
+    def __init__(self, *args: int | pg.typing.IntPoint, **kwargs: int) -> None:
+        """Initialize `IntVector2` instance."""
+        if args:
+            if len(args) > 2:  # noqa: PLR2004
+                err_msg = f"Expected 1 or 2 args, got {len(args)}"
+                raise TypeError(err_msg)
+
+            if len(args) == 1 and not isinstance(args[0], int):
+                try:
+                    super().__setattr__("x", args[0][0])
+                    super().__setattr__("y", args[0][1])
+                except TypeError as err:
+                    raise TypeError from err
+
+            if not isinstance(args[0], int) or not isinstance(args[1], int):
+                err_msg = f"Expected `int`s; got ({type(args[0])}, {type(args[1])})"
+                raise TypeError(err_msg)
+
+            super().__setattr__("x", args[0])
+            super().__setattr__("y", args[1])
+
+        elif kwargs:
+            super().__setattr__("x", kwargs["x"])
+            super().__setattr__("y", kwargs["y"])
 
     @property
-    def as_vector2(self) -> Vector2:
+    def as_vector2(self) -> pg.Vector2:
         """Return `pygame.Vector2`, i.e. float coordinates.
 
         For compatibility with Pygame functions.
         """
-        return Vector2(self.x, self.y)
-
-    def __len__(self) -> int:
-        return len(fields(self))
+        return pg.Vector2(self.x, self.y)
 
     def __add__(self, other: IntVector2 | tuple[int, int]) -> IntVector2:
-        _ensure_2_elements(other)
         if isinstance(other, IntVector2):
             return IntVector2(self.x + other.x, self.y + other.y)
 
         return IntVector2(self.x + other[0], self.y + other[1])
 
-    def __radd__(self, other: tuple[int, int]) -> IntVector2:
-        _ensure_2_elements(other)
-        return IntVector2(other[0] + self.x, other[1] + self.y)
-
     def __sub__(self, other: IntVector2 | tuple[int, int]) -> IntVector2:
-        _ensure_2_elements(other)
         if isinstance(other, IntVector2):
             return IntVector2(self.x - other.x, self.y - other.y)
 
         return IntVector2(self.x - other[0], self.y - other[1])
-
-    def __rsub__(self, other: tuple[int, int]) -> IntVector2:
-        _ensure_2_elements(other)
-        return IntVector2(other[0] - self.x, other[1] - self.y)
 
     def __mul__(self, other: int) -> IntVector2:
         return IntVector2(other * self.x, other * self.y)
