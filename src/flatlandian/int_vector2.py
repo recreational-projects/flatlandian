7d8dce3e591ae7a37c95001253537fd00e985e2d
recreational-projects <205627218+recreational-projects@users.noreply.github.com>
2025-11-24 09:53:30 +0000

    wip

--- src/flatlandian/int_vector2.py
+++ src/flatlandian/int_vector2.py
@@ -2,127 +2,90 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from dataclasses import dataclass
+from typing import TYPE_CHECKING, Sequence
 
-from attrs import define, field, fields, validators
-from pygame.math import Vector2
+import pygame as pg
 
 if TYPE_CHECKING:
-    from collections.abc import Sized
-
-    from pygame.typing import IntPoint
+    from collections.abc import Iterable
 
 
-def _ensure_2_elements(value: Sized) -> bool:
-    """Used in arithmetic operations, not constructors."""
-    if len(value) != 2:  # noqa: PLR2004
-        err_msg = f"Expected 2 elements, got {len(value)}: {value}"
-        raise TypeError(err_msg)
-
-    return True
-
-
-@define(frozen=True)
+@dataclass(frozen=True)
 class IntVector2:
     """A 2-dimensional integer vector.
 
-    Frozen and hashable (unlike pygame.Vector2).
-    Supports `len` (always 2) and indexing.
-    Conforms to `pygame.typing.IntPoint`.
+    `IntVector2() -> IntVector2(0, 0)`
 
-    Constructors
-    ------------
-    ```
-    IntVector2() -> IntVector2(0, 0)
-    IntVector2(int, int) -> IntVector2
-    IntVector2(x: int, y: int) -> IntVector2
-    ```
+    `IntVector2(x: int, y: int) -> IntVector2`
 
-    Attempting to construct with non-`int` values raises `TypeError`.
+    `IntVector2(int, int) -> IntVector2`
 
-    Equivalents to these `pygame.math.Vector2` constructors aren't yet supported:
-    ```
-    Vector2(int) -> Vector2
-    Vector2(Vector2) -> Vector2
-    Vector2((x, y)) -> Vector2
-    ```
-    Arithmetic
-    ----------
-    Supports element-wise addition and subtraction of another `IntVector2`
-    or `pygame.typing.IntPoint` i.e. a sequence-like of 2 `int`s.
-    Returns `IntVector2`.
+    `IntVector2((int, int)) -> IntVector2`
 
-    Supports scalar multiplication and floor division by `int`.
-    Returns `IntVector2`.
+    Attempting to construct with `float` parameters raises `TypeError`.
+
+    Equivalents to these `pygame.Vector2` constructors aren't yet supported:
+
+    `Vector2(int) -> Vector2`
+
+    `Vector2(Vector2) -> Vector2`
     """
 
-    x: int = field(default=0, validator=validators.instance_of(int))
-    """x coordinate. Also available via index 0 or -1."""
+    x: int = 0
+    """x coordinate."""
+    y: int = 0
+    """y coordinate."""
 
-    y: int = field(default=0, validator=validators.instance_of(int))
-    """y coordinate. Also available via index 1 or -2."""
 
-    @classmethod
-    def from_point(cls, value: IntPoint) -> IntVector2:
-        """Construct `IntVector2` from a sequence of two `int`s.
 
-        TODO: Re-implement in IntVector2().`
-        """
-        _ensure_2_elements(value)
-        return cls(value[0], value[1])
+    def __init__(self, *args: int | pg.typing.IntPoint, **kwargs: int) -> None:
+        """Initialize `IntVector2` instance."""
+        if args:
+            if len(args) > 2:  # noqa: PLR2004
+                err_msg = f"Expected 1 or 2 args, got {len(args)}"
+                raise TypeError(err_msg)
 
-    def __repr__(self) -> str:
-        return self.__class__.__name__ + f"({self.x}, {self.y})"
+            if len(args) == 1 and not isinstance(args[0], int):
+                try:
+                    super().__setattr__("x", args[0][0])
+                    super().__setattr__("y", args[0][1])
+                except TypeError as err:
+                    raise TypeError from err
 
-    def __str__(self) -> str:
-        return f"[{self.x}, {self.y}]"
+            if not isinstance(args[0], int) or not isinstance(args[1], int):
+                err_msg = f"Expected `int`s; got ({type(args[0])}, {type(args[1])})"
+                raise TypeError(err_msg)
 
-    def __len__(self) -> int:
-        return len(fields(self))
+            super().__setattr__("x", args[0])
+            super().__setattr__("y", args[1])
 
-    def __getitem__(self, key: int) -> int:
-        if key in {0, -2}:
-            return self.x
-
-        if key in {1, -1}:
-            return self.y
-
-        error_msg = f"IntVector2 index {key} out of range"
-        raise IndexError(error_msg)
-
-    def __add__(self, other: IntPoint) -> IntVector2:
-        _ensure_2_elements(other)
-        return IntVector2(self.x + other[0], self.y + other[1])
-
-    def __radd__(self, other: IntPoint) -> IntVector2:
-        return self + other
-
-    def __sub__(self, other: IntPoint) -> IntVector2:
-        _ensure_2_elements(other)
-        return IntVector2(self.x - other[0], self.y - other[1])
-
-    def __rsub__(self, other: IntPoint) -> IntVector2:
-        _ensure_2_elements(other)
-        return IntVector2(other[0] - self.x, other[1] - self.y)
-
-    def __mul__(self, other: int) -> IntVector2:
-        return IntVector2(self.x * other, self.y * other)
-
-    def __rmul__(self, other: int) -> IntVector2:
-        return self * other
-
-    def __floordiv__(self, other: int) -> IntVector2:
-        return IntVector2(self.x // other, self.y // other)
+        elif kwargs:
+            super().__setattr__("x", kwargs["x"])
+            super().__setattr__("y", kwargs["y"])
 
     @property
-    def as_vector2(self) -> Vector2:
-        """Return `pygame.math.Vector2`, i.e. float coordinates.
+    def as_vector2(self) -> pg.Vector2:
+        """Return `pygame.Vector2`, i.e. float coordinates.
 
         For compatibility with Pygame functions.
         """
-        return Vector2(self.x, self.y)
+        return pg.Vector2(self.x, self.y)
 
-    @property
-    def xy(self) -> tuple[int, int]:
-        """Return tuple."""
-        return self.x, self.y
+    def __add__(self, other: IntVector2 | tuple[int, int]) -> IntVector2:
+        if isinstance(other, IntVector2):
+            return IntVector2(self.x + other.x, self.y + other.y)
+
+        return IntVector2(self.x + other[0], self.y + other[1])
+
+    def __sub__(self, other: IntVector2 | tuple[int, int]) -> IntVector2:
+        if isinstance(other, IntVector2):
+            return IntVector2(self.x - other.x, self.y - other.y)
+
+        return IntVector2(self.x - other[0], self.y - other[1])
+
+    def __mul__(self, other: int) -> IntVector2:
+        return IntVector2(other * self.x, other * self.y)
+
+    def __floordiv__(self, other: int) -> IntVector2:
+        return IntVector2(self.x // other, self.y // other)
