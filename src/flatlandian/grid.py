cde513a754f5ca662dee51e3b5ed49695caff24f
recreational-projects <205627218+recreational-projects@users.noreply.github.com>
2025-11-29 14:58:42 +0000

    add Grid; docs

---
+++ src/flatlandian/grid.py
@@ -0,0 +1,40 @@
+"""Contains `Grid` class."""
+
+from __future__ import annotations
+
+import itertools
+from dataclasses import dataclass
+from typing import ClassVar
+
+from flatlandian.int_vector2 import IntVector2
+
+
+@dataclass
+class Grid:
+    """A rectangular grid of cells."""
+
+    DIRECTIONS: ClassVar = {
+        IntVector2(x, y)
+        for x, y in itertools.product(range(-1, 2), repeat=2)
+        for y in range(-1, 2)
+        if (x, y) != (0, 0)
+    }
+
+    size: IntVector2
+
+    @property
+    def cells(self) -> set[IntVector2]:
+        """Return all cells."""
+        return {
+            IntVector2(x, y)
+            for x, y in itertools.product(range(self.size.x), range(self.size.y))
+        }
+
+    def is_in_bounds(self, cell: IntVector2) -> bool:
+        """Determine if cell is in bounds."""
+        return (0 <= cell.x < self.size.x) and (0 <= cell.y < self.size.y)
+
+    def neighbors(self, cell: IntVector2) -> set[IntVector2]:
+        """Return the neighbors of the cell."""
+        neighbors = {cell + offset for offset in Grid.DIRECTIONS}
+        return {cell for cell in neighbors if self.is_in_bounds(cell)}
---
+++ tests/test_grid.py
@@ -0,0 +1,49 @@
+"""Tests for `Grid` class."""
+
+from flatlandian.grid import Grid
+from flatlandian.int_vector2 import IntVector2
+
+
+def test_create() -> None:
+    # arrange
+    # act
+    grid = Grid(IntVector2(2, 2))
+    # assert
+    assert grid.cells == {
+        IntVector2(0, 0),
+        IntVector2(0, 1),
+        IntVector2(1, 0),
+        IntVector2(1, 1),
+    }
+
+
+def test_neighbors() -> None:
+    # arrange
+    grid = Grid(IntVector2(3, 3))
+    # act
+    ns = grid.neighbors(IntVector2(1, 1))
+    # assert
+    assert len(ns) == 8
+    assert ns == {
+        IntVector2(0, 0),
+        IntVector2(0, 1),
+        IntVector2(0, 2),
+        IntVector2(1, 0),
+        IntVector2(1, 2),
+        IntVector2(2, 0),
+        IntVector2(2, 1),
+        IntVector2(2, 2),
+    }
+
+
+def test_neighbors_corner_cell() -> None:
+    # arrange
+    grid = Grid(IntVector2(2, 2))
+    # act
+    ns = grid.neighbors(IntVector2(0, 0))
+    # assert
+    assert ns == {
+        IntVector2(0, 1),
+        IntVector2(1, 0),
+        IntVector2(1, 1),
+    }
